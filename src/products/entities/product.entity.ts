cf76500c3b61ca83d8152526f8e3a4f12f92afcf
Franco <fmangeleri@gmail.com>
2023-02-28 13:38:54 -0300

    indexadores

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,7 +9,7 @@ export class Product extends Document {
   @Prop()
   description: string;
 
-  @Prop({ type: Number, required: true })
+  @Prop({ type: Number, required: true, index: true })
   price: number;
 
   @Prop({ type: Number, required: true })
@@ -20,3 +20,4 @@ export class Product extends Document {
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
+ProductSchema.index({ price: 1, stock: -1 });
